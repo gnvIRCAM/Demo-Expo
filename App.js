68c68ff436ae921d956c944331e6c4ebf63325e8
genova <genovadavid44@gmail.com>
2024-03-08 18:04:04 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';  
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [location, setLocation] = useState(); 
+
+  useEffect(() => {
+    const getLocationAsync =  async () => {
+      let {status} = await Location.requestForegroundPermissionsAsync(); 
+      if (status==='granted') {
+        let position = await Location.getCurrentPositionAsync()
+        setLocation(position);  
+      }
+      else {
+        console.log('Permission not granted'); 
+      }
+    }
+    getLocationAsync(); 
+  }, [])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MapView 
+      style={styles.container}
+      provider={MapView.PROVIDER_GOOGLE}
+      initialRegion={{
+        latitude: 48,
+        longitude: 2,
+        latitudeDelta: 0.0922*1.2,
+        longitudeDelta: 0.0421*1.2
+      }}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
